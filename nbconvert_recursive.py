b7b4d5652c4e47b027e057b701e428e0b23a3d28
Nathaniel Morck Beaver <nathanielmbeaver@gmail.com>
2022-03-01 01:00:13 -0500

    Remove unnecessary debug code.

--- nbconvert_recursive.py
+++ nbconvert_recursive.py
@@ -8,7 +8,5 @@ filepath = sys.argv[1]
 #notebook = nbformat.from_filename(filepath)
 html_exporter = HTMLExporter(template_name = 'classic')
 (html, resources) = html_exporter.from_filename(filepath)
-#import ipdb
-#ipdb.set_trace()
 with open('out.html', 'w') as fp:
     fp.write(html)
